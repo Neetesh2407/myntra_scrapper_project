5e2808a8489bf3c2f506efe6918df39a2b680f6c
Neetesh2407 <er.neeteshkumar236656@gmail.com>
2025-05-29 12:33:22 +0530

    push the files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "scrapper",
+    version = "0.0.1",
+    author = "neetesh",
+    author_email = "er.neeteshkumar236656@gmail.com",
+    packages = find_packages(),
+    install_requires =[]
+    
+)
